d02bd010a99a0b32d7cf3c39148d1440e33a772d
Artur Uvarov <artur.uff@gmail.com>
2023-11-25 21:27:17 -0500

    added some logic

---
+++ Zonk/Views/ZonkView.swift
@@ -0,0 +1,18 @@
+//
+//  ZonkView.swift
+//  Zonk
+//
+//  Created by Artur Uvarov on 11/25/23.
+//
+
+import SwiftUI
+
+struct ZonkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ZonkView()
+}
